1a5cfd2391b1f846ca358299cd510ef8554f8845
Sangita <yadavsm754@gmail.com>
2025-08-19 22:06:53 +0530

    Terraform-project

--- main.tf
+++ main.tf
@@ -81,22 +81,11 @@ resource "aws_instance" "web" {
 # S3 Bucket
 resource "aws_s3_bucket" "static_files" {
   bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_acl" "static_files_acl" {
+  bucket = aws_s3_bucket.static_files.id
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "public_policy" {
-  bucket = aws_s3_bucket.static_files.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = ["${aws_s3_bucket.static_files.arn}/*"]
-      }
-    ]
-  })
-}
+  
