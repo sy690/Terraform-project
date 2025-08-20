9ec542acf82a9795b248a5d420a169d6e271d584
Sangita <yadavsm754@gmail.com>
2025-08-20 12:36:48 +0530

    terraform-project

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.id   # ✅ reference Terraform VPC
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 }
