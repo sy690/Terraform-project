accc6727607b706d7bd529de51e2d9079e667921
Sangita <yadavsm754@gmail.com>
2025-08-20 13:13:49 +0530

    terraform-project

--- main.tf
+++ main.tf
@@ -1,40 +1,53 @@
-#Create VPC
+# Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "Terraform-VPC"
   }
 }
-#subnet in vpc
+
+# Create Public Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id   # ✅ dynamic reference
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"   # ✅ Always specify AZ
+  map_public_ip_on_launch = true            # ✅ Auto-assign public IPs
+
   tags = {
     Name = "Terraform-Public-Subnet"
   }
 }
 
-
 # Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Terraform-IGW"
+  }
 }
 
-# Route Table
-resource "aws_route_table" "rt" {
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+
+  tags = {
+    Name = "Terraform-Public-RT"
+  }
 }
 
-resource "aws_route_table_association" "a" {
+# Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 # Security Group
